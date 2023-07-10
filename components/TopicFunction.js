10a0449342304e2f555881f5477739bc4d610a63
thulieblack <sibanda.thulie@gmail.com>
2023-07-10 13:51:18 +0200

    add-fallback-mech

--- components/TopicFunction.js
+++ components/TopicFunction.js
@@ -7,13 +7,21 @@ export function TopicFunction({ channels }) {
     const topicsDetails = getTopics(channels);
     let functions = '';
 
-    topicsDetails.forEach(t => {
-      functions += `def send${ t.name }(self, id):
-        topic = "${t.topic}"
-        self.client.publish(topic, id)\n`
-    });
+topicsDetails.forEach(t => {
+    let functionName = '';
+    if (t.name) {
+        functionName = t.name.charAt(0).toUpperCase() + t.name.slice(1);
+    } else {
+        functionName = t.topic.split('/').pop().replace(/[^a-zA-Z0-9]/g, '');
+    }
 
-    return functions;
+    functions += `def send${functionName}(self, id):
+        topic = "${t.topic}"
+        self.client.publish(topic, id)\n`;
+});
+
+return functions;
+
 }
 
 /*
@@ -23,18 +31,32 @@ export function TopicFunction({ channels }) {
  * 
  * As input it requires a list of Channel models from the parsed AsyncAPI document
  */ 
+
+
 function getTopics(channels) {
     const channelsCanSendTo = channels;
     let topicsDetails = [];
 
     channelsCanSendTo.forEach(ch => {
       const topic = {};
-      const operationId = ch.operations().filterByReceive()[0].id();
-      topic.name = operationId.charAt(0).toUpperCase() + operationId.slice(1);
+      const operation = ch.operations().filterByReceive()[0];
+  
+      if (operation && operation.id()) {
+          topic.name = operation.id();
+      } else {
+          topic.name = null;
+      }
+  
       topic.topic = ch.address();
-
+  
       topicsDetails.push(topic);
-    });
-
-    return topicsDetails;
+  });
+  
+  return topicsDetails;
+  
 }
+
+/*
+ * getTopics checks if there is an operation and an id.
+ * If there isn't any operatonId it fallsback to using channel name to generate a topic name
+ */ 
