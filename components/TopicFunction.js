ad1dcea2534604fcc445d1882f2fc15f47f061f1
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2023-07-19 17:25:19 +0200

    feat: add fallback mechanism for `operationId` (#11)
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>

--- components/TopicFunction.js
+++ components/TopicFunction.js
@@ -1,40 +1,61 @@
 /*
  * This component returns a blok of functions that user can use to send messages to specific topic.
- * As input it requires a list of Channel models from the parsed AsyncAPI document
- */ 
+ * As input it requires a list of channel models from the parsed AsyncAPI document
+
+*/
+
 export function TopicFunction({ channels }) {
+  const topicsDetails = getFunctionDetails(channels);
+  let functions = '';
 
-    const topicsDetails = getTopics(channels);
-    let functions = '';
+  topicsDetails.forEach(t => {
 
-    topicsDetails.forEach(t => {
-      functions += `def send${ t.name }(self, id):
-        topic = "${t.topic}"
-        self.client.publish(topic, id)\n`
-    });
+    functions += `def ${t.functionName}(self, id):
+      topic = "${t.topic}"
+      self.client.publish(topic, id)\n`;
+  });
 
-    return functions;
+  return functions;
+}
+
+
+/* the function takes a string and capitalizes the words after the first '/'
+*/
+
+function capitalizeWords(str) {
+  return str.split('/').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
 }
 
 /*
- * This function returns a list of objects, one for each channel with two properties, name and topic
- * name - holds information about the operationId provided in the AsyncAPI document
- * topic - holds information about the address of the topic
- * 
- * As input it requires a list of Channel models from the parsed AsyncAPI document
- */ 
-function getTopics(channels) {
-    const channelsCanSendTo = channels;
-    let topicsDetails = [];
+* This function returns a list of objects, one for each channel with two properties, functionName and topic
+* functionName - holds the name of the function built from operationId or channel address if operationId is missing
+* topic - holds information about the address of the topic
+* 
+* As input it requires a list of channels properties from the parsed AsyncAPI document
+*/ 
 
-    channelsCanSendTo.forEach(ch => {
-      const topic = {};
-      const operationId = ch.operations().filterByReceive()[0].id();
-      topic.name = operationId.charAt(0).toUpperCase() + operationId.slice(1);
-      topic.topic = ch.address();
+function getFunctionDetails(channels) {
+  const channelsCanSendTo = channels;
+  let topicsDetails = [];
 
-      topicsDetails.push(topic);
-    });
+  channelsCanSendTo.forEach(ch => {
+    const topic = {};
+    const operation = ch.operations().filterByReceive()[0];
 
-    return topicsDetails;
+    let capitalizedName = '';
+
+    if (operation.hasOperationId()) {
+      capitalizedName = capitalizeWords(operation.id());
+    } else {
+      capitalizedName = capitalizeWords(ch.address());
+    }
+
+    topic.functionName = "send" + capitalizedName;
+    topic.topic = ch.address();
+
+    topicsDetails.push(topic);
+  });
+
+  return topicsDetails;
 }
+
