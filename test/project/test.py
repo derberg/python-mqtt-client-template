e7d579b23aca51ec2a49a0f13a1784bdbc6b5f53
thulieblack <sibanda.thulie@gmail.com>
2024-02-23 08:24:20 +0200

    update-changes

--- test/project/test.py
+++ test/project/test.py
@@ -12,16 +12,16 @@ def read_views(client, user_data, message):
     print("Received message on topic: " + message.topic)
     print("Message: " + str(message.payload.decode()))
 
-client.commentViews(read_views)
+client.receiveCommentViews(read_views)
 
 # Define a function for sending comments liked and unliked
 def send_comments():
     while True:
         randomId = randrange(min_value, max_value + 1)
-        client.commentLiked(randomId) 
+        client.sendCommentLiked(randomId) 
         print("New like for comment " + str(randomId) + " sent to comment/liked")
         
-        client.commentUnliked(randomId)
+        client.sendCommentUnliked(randomId)
         print("Comment " + str(randomId) + " unliked info sent to comment/unliked")
         
         time.sleep(1)
