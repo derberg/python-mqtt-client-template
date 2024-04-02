d144bd27d28549d81fd38a771bae99abcf7c0eeb
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2024-04-02 10:53:52 +0200

    feat: add docstring to the functions  (#15)

--- components/OperationFunction.js
+++ components/OperationFunction.js
@@ -1,23 +1,33 @@
-//Generates and returns a string containing send functions based on provided operations in the yaml file.
+//Generates and returns a string containing send functions based on provided operations in the asyncapi file.
 
-export function GenerateSendFunctions({ operations }) {
+export function GenerateSendFunctions({ operations, className}) {
   const sendOperations = Array.from(operations).filter(op => op.isSend());
   const sendDetails = getFunctionDetails(sendOperations);
   let functions = '';
 
   sendDetails.forEach(t => {
-    // Generate send function 
+    // Send a message to the MQTT broker on whether a comment is liked or unliked by someone.
     functions += `def ${t.functionName}(self, id):
-        topic = "${t.topic}"
-        self.client.publish(topic, id)\n`;
+    """
+    ${t.summary}
+
+    Args:
+
+      self: ${className}
+
+      id (str) : Comment ID
+    
+    """
+    topic = "${t.topic}"
+    self.client.publish(topic, id)\n`;
   });
 
   return functions;
 }
 
-//Generates and returns a string containing receive functions based on provided operations in the yaml file.
+// Generates and returns a string containing receive functions based on provided operations in the asyncapi file.
 
-export function GenerateReceiveFunctions({ operations }) {
+export function GenerateReceiveFunctions({ operations, className }) {
   const receiveOperations = Array.from(operations).filter(op => op.isReceive());
   const receiveDetails = getFunctionDetails(receiveOperations);
   let functions = '';
@@ -25,15 +35,34 @@ export function GenerateReceiveFunctions({ operations }) {
   receiveDetails.forEach(t => {
     // Generate receive function
     functions += `def ${t.functionName}(self, callback):
-        topic = "${t.topic}"
-        self.client.subscribe(topic)
-        self.client.message_callback_add(topic, callback)\n`;
+    """
+    ${t.summary}
+
+    Args:
+
+      self: ${className}
+
+      callback: Callback function to handle received messages
+    
+    Callback Args:
+    
+      client: MQTT client instance that triggers the callback.
+
+      userdata: User data associated with the client. It's set when the client is created.
+
+      message: MQTT message instance representing the received message. It contains information such as topic, payload, etc.
+
+    """
+    topic = "${t.topic}"
+    self.client.subscribe(topic)
+    self.client.message_callback_add(topic, callback)\n`;
   });
   
   return functions;
 }
 
-//     Capitalizes the first letter of each word in a given string. Follows the camelCase rule.
+// Capitalizes the first letter of each word in a given string. Follows the camelCase rule.
+
 function capitalizeWords(str) {
   return str.split('/').map(word => word.charAt(0).toLowerCase() + word.slice(1)).join('');
 }
@@ -50,6 +79,7 @@ function getFunctionDetails(operations) {
       topic.functionName = capitalizedName;
       const channel = operation.channels();
       topic.topic = channel[0].address();
+      topic.summary = operation.summary();
 
       details.push(topic);
   });
